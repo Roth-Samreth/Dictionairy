40b26bdc8b289cdb9fef51698941fc774b4dcb73
roth <rothsamreth@gmail.com>
2023-09-12 18:52:15 +0700

    correction

--- Exercise.py
+++ Exercise.py
@@ -4,13 +4,17 @@ studentRecord = [
     {"studentName":"Villa","class":" wep-a","algorithm":96,"html":92},
     {"studentName":"mengheang","class":"wep-a","algorithm":66,"html":54},
 ]
+def countA(arr):
+    sum = 0
+    for i in range(len(studentRecord)):
+        if studentRecord[i]["class"]=="wep-a":
+            sum += 1
+    return sum
+wepA = countA(studentRecord)
 sum = 0
 avg  = 0
-res = []
 for i in range(len(studentRecord)):
-    sum = 0
     if studentRecord[i]["class"]=="wep-a":
         sum += studentRecord[i]['algorithm']
-        avg = sum/2
-        res.append(studentRecord[i]["studentName"]+": "+str(avg))
-print(res)
+avg = sum/wepA
+print(avg)
