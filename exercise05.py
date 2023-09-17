4987b4465c42fb563f88c77e4e0d6c9de712dc4d
roth <rothsamreth@gmail.com>
2023-09-17 11:05:12 +0700

    message

---
+++ exercise05.py
@@ -0,0 +1,62 @@
+# Ex9 - Array - Object or array dictionary
+# fruits = [
+#   {'bmc': ['banana','coconut', 'apple','mango']},
+#   {'btb': ['orange', 'banana']},
+#   {'sr': ['jackfruit','coconut', 'banana']}
+# ]
+# #1 - Which province has fruit the most?
+# #2 - Which province has coconut
+# [
+#   {'province': 'bmc'},
+#   {'province': 'sr'}  
+# ]
+# #3 - How many banana in fruit list
+# {'banana': 3}
+# #4 - add mango to btn province
+# Ex1 - Array
+# input1: ['banana','coconut','mango']
+# input2: ['a','o']
+# output:
+# {'a': 4, 'o': 3]
+# Ex2 - Array
+# # Find index
+# input1: [3, 3, 4, 5, 6, 6]
+# input2: [3, 4, 6]
+# ouput: {3: "01", 4: "2", 6:"45"}
+# Ex3 - Array
+# #Find index
+# input1: [3, 3, 4, 5, 6, 6]
+# input2: [3, 4, 6]
+# ouput: {3: [0, 1], 4: 2, 6:[4, 5]}
+# Ex4 - Array
+# #Get value by index
+# input1: ['banana','coconut','mango']
+# input2: [0, 2]
+# output:
+# {0: 'banana', 2: 'mango'}
+# Ex6 - object
+# input1: ['banana','coconut','mango']
+# input2: [0,1]
+# [
+#   {0:'b',1:'a', 2:'n', 3: 'a', 4: 'n',5:'a'},
+#   {0:'m',1:'a', 2:'n', 3: 'g', 4: 'o'},
+# ]
+# Ex7 - Array 2D
+# input: [3,4, 5, 1]
+# ouput: 
+# [
+#   [1, 2, 3],
+#   [1, 2, 3, 4],
+#   [1, 2, 3, 4, 5],
+#   [1]
+# ]
+# Ex8 - Object
+# #Reverse Object
+# input: {1: 'banana', 2: 'mango', 3: 'coconut'}
+# output: {1: 'ananab', 2: 'ognam', 3: 'tunococ'}
+# Ex9 - Array
+# #Replace character by something
+# input1: ['banana','coconut','mango']
+# input2: ['a', '*']
+# output:
+# ['b*n*n*','coconut','m*ngo']
